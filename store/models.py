14e937f151d117370babe137b81e786eba5f3ddd
Chi Tran <chi.tran@veriksystems.com>
2024-10-19 11:04:01 +0700

    add details

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
 	name = models.CharField(max_length = 100)
 	price = models.DecimalField(default = 0, decimal_places =2, max_digits = 8)
 	category = models.ForeignKey(Category, on_delete = models.CASCADE, default = 1)
-	description = models.CharField(max_length = 50, default = '', blank = True, null = True)
+	description = models.CharField(max_length = 500, default = '', blank = True, null = True)
 	image = models.ImageField(upload_to = 'uploads/product/')
 	is_sale = models.BooleanField(default = False)
 	sale_price = models.DecimalField(default = 0, decimal_places = 2, max_digits = 8)
