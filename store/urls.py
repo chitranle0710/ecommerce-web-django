9262d77a5508930e8d65342d35b7eb761b129296
Chi Tran <chi.tran@veriksystems.com>
2024-10-20 06:53:08 +0700

    update user profile

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name = 'about'),
     path('login/', views.login_user, name = 'login'),
     path('logout/', views.logout_user, name = 'logout'),
+    path('update_user/', views.update_user, name = 'update_user'),
     path('register/', views.register_user, name = 'register'),
     path('product/<int:pk>', views.product, name = 'product'),
     path('category/<str:foo>', views.category, name = 'category'),
