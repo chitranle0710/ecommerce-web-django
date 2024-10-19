dd7210d1fe1acab638fd840f82992fff1ec08055
Chi Tran <chi.tran@veriksystems.com>
2024-10-19 14:30:29 +0700

    add item to cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+
 class Cart():
 	def __init__(self, request):
 		self.session = request.session;
@@ -7,4 +8,16 @@ class Cart():
 		if 'session_key' not in request.session:
 			cart = self.session['session_key'] = {}
 
+
 		self.cart = cart
+
+	def add(self, product):
+		product_id = str(product.id)
+
+		if product_id in self.cart:
+			pass
+		else:
+			self.cart['product_id'] = {'price': str(product.price)}
+
+		self.session.modified = True
+
--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+from django.http import JsonResponse
+from .cart import Cart
 
 # Create your views here.
 def cart_summary(request):
-	return render(request, 'cart_summary.html',{})
+    return render(request, 'cart_summary.html', {})
 
 def cart_add(request):
-	pass
+    cart = Cart(request)
 
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        response = JsonResponse({'Product Name': product.name})  # Fixed key name syntax
+        return response
+        
 def cart_delete(request):
-	pass
+    pass  # Placeholder for cart delete functionality
 
 def cart_update(request):
-	pass
+    pass  # Placeholder for cart update functionality
