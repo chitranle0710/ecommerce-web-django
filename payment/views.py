2df57535d26bfa1fc62a48a8e9675fecde78f5ee
Chi Tran <chi.tran@veriksystems.com>
2024-10-20 14:16:57 +0700

    add billing view

--- payment/views.py
+++ payment/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
+from django.contrib import messages
+from store.models import Product, Profile
+import datetime
 
+# Import Some Paypal Stuff
+from django.urls import reverse
+from django.conf import settings
+import uuid # unique user id for duplictate orders
 
 # Create your views here.
+# Check to see if user is logged in
+def billing_info(request):
+	if request.POST:
+		# Get the cart
+		cart = Cart(request)
+		cart_products = cart.get_prods
+		quantities = cart.get_quants
+		totals = cart.cart_total()
+
+		if request.user.is_authenticated:
+			# Get The Billing Form
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+
+		else:
+			# Not logged in
+			# Get The Billing Form
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+
+		shipping_form = request.POST
+		return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})	
+	else:
+		messages.success(request, "Access Denied")
+		return redirect('home')
 
 
 def checkout(request):
